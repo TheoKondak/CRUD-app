a1b3eb1079365f8061105e625ff3d93c40cc2ebf
TheoKondak <thkondak@gmail.com>
2023-03-08 18:02:36 +0100

    chore: disabled stringMode for now

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // Removed Strict mode for now, so that codeNinja wont display duplicate clg
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
