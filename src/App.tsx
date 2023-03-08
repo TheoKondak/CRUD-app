93ca73a9238c779bb69f1894a9ac9ce9ee7067cf
TheoKondak <thkondak@gmail.com>
2023-03-08 14:02:28 +0100

    chore: Original update - Updated `packages.json` with required packages - Added configuration files for the packages - Removed unnecessary files, code - Created a basic DB file scaffold - Added tapio logo .png, converted it to .svg - Added `.env` for the server address

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return <div className="posts">Hello World</div>;
+}
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,27 @@
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.cyan,
+        secondary: colors.teal,
+        tertiary: colors.violet,
+        white: '#ffffff',
+        black: '#000000',
+      },
+      fontSize: {
+        xs: '14px',
+        xxs: '13px',
+        xxxs: '12px',
+      },
+      fontFamily: {
+        'roboto-condensed': ['"Roboto Condensed"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+};
