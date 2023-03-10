268c70c7de43c965200f06d49adf0bd902d47194
TheoKondak <thkondak@gmail.com>
2023-03-10 16:45:02 +0100

    chore: removed obsolete code

--- src/services/postsService.ts
+++ src/services/postsService.ts
@@ -25,23 +25,13 @@ const get = (query: string) => {
     });
 };
 
-const create = (newObject, notification = { message: 'No Message Selected', duration: 5000 }) => {
-  const request = axios.post(baseUrl, newObject);
+const create = (newObject) => {
+  const request = axios.post(`${baseUrl}/posts`, newObject);
   return request
     .then((response) => {
-      // notification.setNotificationMessage(notification.message);
-      setTimeout(() => {
-        // notification.setNotificationMessage(null);
-      }, notification.duration);
-
       return response.data;
     })
     .catch((error) => {
-      // notification.setNotificationMessage('Failed to create new post');
-      setTimeout(() => {
-        // notification.setNotificationMessage(null);
-      }, notification.duration);
-
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
